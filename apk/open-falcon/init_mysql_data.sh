e0b83a4310f2984f75ff9c0409a1d6a84be4ce1e
bbxyard <bbxyard@gmail.com>
2019-11-03 15:07:50 +0800

    优化掉open-falcon初始化脚本

--- apk/open-falcon/init_mysql_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mysql_pod=$(kubectl get pods -n baltic | grep mysql | awk '{print $1}')
-for x in `ls ./stub/*.sql`; do
-	echo init mysql table $x ...;
-	kubectl -n baltic exec -it $mysql_pod -- mysql -uroot -p123456 < $x;
-done
